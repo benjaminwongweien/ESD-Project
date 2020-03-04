c7aa5569eea831be1dd68ecb50d4a489ab3c44e1
benjaminwongweien <54574922+benjaminwongweien@users.noreply.github.com>
2020-03-04 22:03:41 +0800

    Added Menu Basic and Full Menu Options
    This ONLY works when you use flask run

--- menu/app.py
+++ menu/app.py
@@ -6,29 +6,136 @@ Menu Microservice
 @Team   - G3T4
 """
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from model.base import db
 from model.data_models import Vendor, Food
+from sqlalchemy_utils import database_exists, create_database
 
-def create_app(user,ip,port,dbname,password=""):
-  
+def create_app(uri):
   """
   Creates and starts the App with all the required settings
   mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
   """
-  
   app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{user}:{password}@{ip}:{port}/{dbname}"
+  app.config['SQLALCHEMY_DATABASE_URI'] = uri
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  # create the database menu if it does not exist
+  if not database_exists(uri):
+    create_database(uri)
   db.init_app(app)
   return app
 
-app = create_app(os.environ['USER'],os.environ['IP'],os.environ['PORT'],os.environ['DBNAME'])
-# https://flask-sqlalchemy.palletsprojects.com/en/2.x/contexts/
-app.app_context().push()
+app = create_app(os.environ['URI'])
+app.app_context().push() # https://flask-sqlalchemy.palletsprojects.com/en/2.x/contexts/
+db.create_all() # Creates All Tables 
 
-Hello = Vendor("Ben",True)
-db.session.add(Hello)
-db.session.commit()
+# Please disable by commenting off app.route -- For Testing Purposes Only
+@app.route("/all_vendors", methods=["GET"])
+def all_vendors():
+  """
+  Obtains JSON of all Vendors
+  """
+  output = {"status": ""}
+  try:
+    vendors = Vendor.query.all()
+    output['status'] = "success"
+    output['vendors'] = [vendor.json() for vendor in vendors]
+    return jsonify(output), 200
+  except:
+    output['status'] = "error"
+    return jsonify(output), 400 
 
-# add other db stuff here flask app shld kinda be ready hope so XD
+# Please disable by commenting off app.route -- For Testing Purposes Only
+@app.route("/all_food", methods=["GET"])
+def all_food():
+  """
+  Obtains JSON of all Available Food
+  """
+  output = {"status": ""}
+  try:
+    foods = Food.query.all()
+    output['status'] = "success"
+    output['vendors'] = [food.full_json() for food in foods]
+    return jsonify(output), 200
+  except:
+    output['status'] = "error"
+    return jsonify(output), 400
+
+@app.route("/menu", methods=["GET"])
+def menu():
+  """
+  Obtains the FULL Menu
+  
+  Structure:
+  
+  { status:
+    vendors: [
+      {
+        vendor_id:
+        vendor_name:
+        halal:
+        foods: [
+          {
+            food_id:
+            food_name:      
+            food_description:
+            food_price:
+          } 
+        ]
+      }
+    ]
+  }
+  """
+  output = {"status": ""}
+  try:
+    vendor_list = []
+    vendors = Vendor.query.all()
+    for vendor in vendors:
+      foods = Food.query.filter_by(vendor_id=vendor.vendor_id, availability=True, listed=True)
+      vendor_json = vendor.json()
+      vendor_json['foods'] = [food.json() for food in foods]
+      vendor_list.append(vendor_json)
+    output['status'] = "success"
+    output['vendors'] = vendor_list
+    return jsonify(output), 200
+  except:
+    output['status'] = "error"
+    return jsonify(output), 400
+  
+@app.route("/menu_basic", methods=["GET"])
+def menu_basic():
+  """
+  Obtains the Basic Menu
+  
+  Structure:
+  
+  { status:
+    vendors: [
+      {
+        vendor_id:
+        vendor_name:
+        foods: [
+          {
+            food_id:
+            food_name:     
+          } 
+        ]
+      }
+    ]
+  }
+  """
+  output = {"status": ""}
+  try:
+    vendor_list = []
+    vendors = Vendor.query.all()
+    for vendor in vendors:
+      foods = Food.query.filter_by(vendor_id=vendor.vendor_id, availability=True, listed=True)
+      vendor_json = vendor.json_basic()
+      vendor_json['foods'] = [food.json_basic() for food in foods]
+      vendor_list.append(vendor_json)
+    output['status'] = "success"
+    output['vendors'] = vendor_list
+    return jsonify(output), 200
+  except:
+    output['status'] = "error"
+    return jsonify(output), 400
--- menu/model/data_models.py
+++ menu/model/data_models.py
@@ -1,38 +1,69 @@
+##!/usr/bin/python
+
+"""
+Menu Microservice Data Models
+@Author - Benjamin Wong Wei En, Hao Jun Poon, Belle Lee, Chen Ziyi, Masturah Binte Sulaiman
+@Team   - G3T4
+"""
 from model.base import db
 
 class Vendor(db.Model):
-    vendor_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    vendor_name = db.Column(db.String(80), nullable=False)
-    halal = db.Column(db.Boolean, nullable=False)
+    vendor_id       = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    vendor_name     = db.Column(db.String(80), nullable=False)
+    halal           = db.Column(db.Boolean, nullable=False)
     # We point to the Food class
-    menu_foods = db.relationship("Food", backref="Vendor", lazy=True)
+    menu_foods      = db.relationship("Food", backref="Vendor", lazy=True)
     
     def __init__(self,vendor_name,halal):
         self.vendor_name = vendor_name
-        self.halal = halal
+        self.halal       = halal
     
-    def __str__(self):
-        return dir(self)
+    def json_basic(self):
+        return {"vendor_id":   self.vendor_id,
+                "vendor_name": self.vendor_name}        
+    
+    def json(self):
+        return {"vendor_id":   self.vendor_id,
+                "vendor_name": self.vendor_name,
+                "halal":       self.halal}
 
 class Food(db.Model):
-    food_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    food_name = db.Column(db.String(80), nullable=False)
+    vendor_id        = db.Column(db.Integer, db.ForeignKey(Vendor.vendor_id), primary_key=True)
+    food_id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    food_name        = db.Column(db.String(80), nullable=False)
     food_description = db.Column(db.Text, nullable=True)
-    food_price = db.Column(db.Float(precision=2), nullable=False)
+    food_price       = db.Column(db.Float(precision=2), nullable=False)
     # Vendor can take item off an order
-    availability = db.Column(db.Boolean, nullable=False, default=True)
+    availability     = db.Column(db.Boolean, nullable=False, default=True)
     # Vendor deletes the item off the menu
-    listed = db.Column(db.Boolean, nullable=False, default=True)
-    vendor_id = db.Column(db.Integer, db.ForeignKey(Vendor.vendor_id), nullable=False)
+    listed           = db.Column(db.Boolean, nullable=False, default=True)
     
     def __init__(self,vendor_id,food_name,food_description,food_price):
-        self.vendor_id = vendor_id
-        self.food_name = food_name
+        self.vendor_id        = vendor_id
+        self.food_name        = food_name
         self.food_description = food_description
-        self.food_price = food_price
+        self.food_price       = food_price
+    
+    def json(self):
+        return {"food_id":          self.food_id,
+                "food_name":        self.food_name,
+                "food_description": self.food_description,
+                "food_price":       self.food_price}    
+    
+    def json_basic(self):
+        return {"food_id":          self.food_id,
+                "food_name":        self.food_name}     
+    
+    def full_json(self):
+        return {"vendor_id":        self.vendor_id,
+                "vendor_name":      self.vendor_name,
+                "food_id":          self.food_id,
+                "food_name":        self.food_name,
+                "food_description": self.food_description,
+                "food_price":       self.food_price,
+                "availability":     self.availability,
+                "listed":           self.listed}
         
-    def __str__(self):
-        return dir(self)
     
 
     
