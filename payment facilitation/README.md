4c30ff9936f56e491c306716aa0f272239a8d8ab
benjaminwongweien <54574922+benjaminwongweien@users.noreply.github.com>
2020-03-08 17:59:35 +0800

    Code Cleaning

--- payment facilitation/README.md
+++ payment facilitation/README.md
@@ -1,6 +1,18 @@
-Visa: 4242 4242 4242 4242
-Mastercard: 5555 5555 5555 4444
-American Express: 3782 822463 10005
+Fake Test Card Details
 
+***Visa***
 
-^^ Test Cards
+4242 4242 4242 4242 Charge is successful.
+
+These errors occur at stripe's side:
+4000 0000 0000 0002 Charge is declined with a card_declined code.
+4000 0000 0000 9995 Charge is declined with a card_declined code. The decline_code attribute is insufficient_funds.
+4000 0000 0000 9987 Charge is declined with a card_declined code. The decline_code attribute is lost_card.
+4000 0000 0000 9979 Charge is declined with a card_declined code. The decline_code attribute is stolen_card.
+4000 0000 0000 0069 Charge is declined with an expired_card code.
+4000 0000 0000 0127 Charge is declined with an incorrect_cvc code.
+4000 0000 0000 0119 Charge is declined with a processing_error code.
+4242 4242 4242 4241 Charge is declined with an incorrect_number code as the card number fails the Luhn check.
+
+***Mastercard***
+5555 5555 5555 4444 Charge is successful.
--- payment facilitation/payment.php
+++ payment facilitation/payment.php
@@ -1,17 +1,14 @@
 <?php
-// if ($_POST) {
-//   // subject to change in the future
-//   if (empty($_POST['vendorid']) || empty($_POST['foodname'])) {
-//     header("location:TEST.html");
-//     exit();
-//   }
-// }
-// else {
-//   header("location:TEST.html");
-//   exit();
-// }
+/* Validation Section of the Code*/
 
-// we need to cURL Menu Microservice for more information but doesn't matter now
+// Validate the POST Request
+  // Code Here
+
+// Validate the contents of the POST Request
+  // we need to cURL Menu Microservice for all the details as only some information is sent over
+  // Code Here
+
+/* The STRIPE Library Dependencies: curl, json, mbstring */
 require_once("./stripe-php-7.27.1/init.php");
 ?>
 
@@ -26,33 +23,40 @@ require_once("./stripe-php-7.27.1/init.php");
 
 <body>
   <?php
+  // This is the Private Key
+  // If deployed, these should be stored as an evironment variable
   Stripe\Stripe::setApiKey('sk_test_IGEllSO26K2ZNmsjykkK0yom00Wh5CGkdw');
 
   $customer = \Stripe\Checkout\Session::create([
-    'success_url' => "http://localhost/run/success.php?customerid={$_POST['customerid']}&session_id={CHECKOUT_SESSION_ID}",
-    'cancel_url' => "http://localhost/run/cancel?customerid={$_POST['customerid']}&session_id={CHECKOUT_SESSION_ID}",
+    'success_url'          => "http://localhost/run/success.php?customerid={$_POST['customerid']}&session_id={CHECKOUT_SESSION_ID}",
+    'cancel_url'           => "http://localhost/run/cancel?customerid={$_POST['customerid']}&session_id={CHECKOUT_SESSION_ID}",
     'payment_method_types' => ['card'],
-    'line_items' => [
+    'line_items'           => [
       [
-        'name' => $_POST['foodname'],
+        'name'        => $_POST['foodname'],
         'description' => 'A very unique description',
-        'amount' => 1000,
-        'currency' => 'sgd',
-        'quantity' => $_POST['quantity'],
+        'images' => ['https://drive.google.com/file/d/1LK7nsufQ-zed17KYJQDx7KHAJ1przBMM/view?usp=sharing'],
+        'amount'      => 1000,
+        'currency'    => 'sgd',
+        'quantity'    => $_POST['quantity'],
       ],
     ],
   ]);
 
+  // Obtain the Checkout/Session ID from the customer object using the getter method
   $id = $customer->getLastResponse()->json['id'];
 
+  // Perform the Javascript Redirect to Checkout
   echo '<script src="https://js.stripe.com/v3/"></script>';
   echo "<script>
           var stripe = Stripe('pk_test_56Qu5vUEGwiUGLHrQ1IZXORf00M3K2og5l');
+
           stripe.redirectToCheckout({
             sessionId: '$id'
           }).then(function (result) {
-            $('.container-fluid').hide();
+            window.location.replace('http://localhost/run/cancel?customerid={$_POST['customerid']}&session_id={CHECKOUT_SESSION_ID}')
           }); 
+
         </script>";
   ?>
   </body>
