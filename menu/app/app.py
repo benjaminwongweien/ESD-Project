164c60a172872989933b1a4b8184f3a933b407ce
benjaminwongweien <54574922+benjaminwongweien@users.noreply.github.com>
2020-03-11 17:11:09 +0800

    Changes to Menu Microservice

--- menu/app/app.py
+++ menu/app/app.py
@@ -5,16 +5,17 @@ Menu Microservice
 """
 import os
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
 from model.base import db
 from model.data_models import Vendor, Food
 
 def create_app(uri):
   """
   Creates and starts the App with all the required settings
-  mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
   """
   app = Flask(__name__)
+  CORS(app)
   app.config['SQLALCHEMY_DATABASE_URI'] = uri
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   db.init_app(app)
@@ -24,71 +25,90 @@ app = create_app(os.environ["URI"])
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/contexts/
 app.app_context().push() 
 
+@app.errorhandler(400)
 @app.errorhandler(404)
+@app.errorhandler(405)
 @app.errorhandler(500)
 @app.errorhandler(502)
 @app.errorhandler(503)
 @app.errorhandler(504)
 def error(e):
-  return jsonify({"error": "error"})
+  return jsonify({"status": "error",
+                  "error" : e.description}), e.code
 
 @app.route("/bootstrap", methods=["GET"])
 def bootstrap():
-  """
-  Reset the Server
-  """
+  """Bootstrap the Server"""
   db.create_all()
   db.session.commit()
   import csv
   with open("menu.csv","r") as file:
     csvfile = csv.reader(file,delimiter=",",quotechar='"')
-    csvfile = list( csvfile)
+    csvfile = list(csvfile)
   
-  for x in range(1,csvfile.count(["","",""]) + 2):
-    db.session.add(Vendor(f"Vendor{x}",True))
+  for x in range(1,6):
+    line = csvfile[x-1]
+    db.session.add(Vendor(line[0],
+                          line[1],
+                          line[2],
+                          "vendor/{}".format(x)))  
   db.session.commit()
 
-  count=1
-  for line in csvfile:
-    if line == ["","",""]:
-      count += 1
+  vendor_id=1
+  food_id=1
+  for x in range(7,36):
+    line = csvfile[x-1]
+    if line[0] == "":
+      vendor_id += 1
     else:
-      db.session.add(Food(count,line[0],line[1],float(line[2])))
+      db.session.add(Food(vendor_id,
+                          line[0],
+                          line[1],
+                          line[2],
+                          line[3],
+                          "food/{}/{}".format(vendor_id,food_id)))
+      food_id += 1
   db.session.commit()
-  
-  return "ok"
-  
-# Please disable by commenting off app.route -- For Testing Purposes Only
-@app.route("/all_vendors", methods=["GET"])
-def all_vendors():
-  """
-  Obtains JSON of all Vendors
-  """
-  output = {"status": ""}
-  try:
-    vendors = Vendor.query.all()
-    output['status'] = "success"
-    output['vendors'] = [vendor.json_full() for vendor in vendors]
-    return jsonify(output), 200
-  except:
-    output['status'] = "error"
-    return jsonify(output), 400 
+  return jsonify({"boostrap": "success"})
 
-# Please disable by commenting off app.route -- For Testing Purposes Only
+@app.route("/all_vendor", methods=["GET"])
+def all_vendors():
+  """ Obtains JSON of all Vendors """
+  output = {"status": ""}
+  vendors = Vendor.query.all()
+  output['status'] = "success"
+  output['vendors'] = [vendor.json(0,1,2,3,4) for vendor in vendors]
+  return jsonify(output), 200
+
 @app.route("/all_food", methods=["GET"])
 def all_food():
-  """
-  Obtains JSON of all Available Food
-  """
-  output = {"status": ""}
-  try:
-    foods = Food.query.all()
-    output['status'] = "success"
-    output['food'] = [food.json_full() for food in foods]
-    return jsonify(output), 200
-  except:
-    output['status'] = "error"
-    return jsonify(output), 400
+  """ Obtains JSON of all Available Food """
+  output = dict()
+  foods = Food.query.all()
+  output['status'] = "success"
+  output['food'] = [food.json(0,1,2,3,4,5,6,7,8) for food in foods]
+  return jsonify(output), 200
+
+@app.route("/images/<path:file_name>", methods=["GET"])
+def staticfiles(file_name):
+  return send_from_directory("./static", file_name)
+
+@app.route("/upload/<string:vendor_id>/<string:food_id>", methods=["POST"])
+def upload_file(vendor_id,food_id):
+    if request.method == "POST":
+        file = request.files["image"]
+        
+        food = Food.query.filter_by(food_id=food_id).update({"food_image": "food/{}/{}".format(vendor_id,food_id)})
+        db.session.commit()
+        
+        file.filename = food_id
+        path = os.path.join("./static/food", vendor_id)
+        if not os.path.exists(path):
+          os.makedirs(path)
+        file.save(os.path.join(path, file.filename))
+        
+    return {"status"      : "success",
+            "image_upload": "success"}
 
 @app.route("/menu", methods=["GET"])
 def menu():
@@ -102,34 +122,35 @@ def menu():
       {
         vendor_id:
         vendor_name:
+        vendor_description:
+        vendor_location:
+        vendor_image:
         halal:
         foods: [
           {
             food_id:
             food_name:      
             food_description:
+            food_image:
             food_price:
+            food_label:
           } 
         ]
       }
     ]
   }
   """
-  output = {"status": ""}
-  try:
-    vendor_list = []
-    vendors = Vendor.query.all()
-    for vendor in vendors:
-      foods = Food.query.filter_by(vendor_id=vendor.vendor_id, availability=True, listed=True)
-      vendor_json = vendor.json_full()
-      vendor_json['foods'] = [food.json_default() for food in foods]
-      vendor_list.append(vendor_json)
-    output['status'] = "success"
-    output['vendors'] = vendor_list
-    return jsonify(output), 200
-  except:
-    output['status'] = "error"
-    return jsonify(output), 400
+  output = dict()
+  vendor_list = []
+  vendors = Vendor.query.all()
+  for vendor in vendors:
+    foods = Food.query.filter_by(vendor_id=vendor.vendor_id, availability=True, listed=True)
+    vendor_json = vendor.json(0,1,2,3,4)
+    vendor_json['foods'] = [food.json(1,2,3,4,5,6) for food in foods]
+    vendor_list.append(vendor_json)
+  output['status'] = "success"
+  output['vendors'] = vendor_list
+  return jsonify(output), 200
   
 @app.route("/menu_basic", methods=["GET"])
 def menu_basic():
@@ -153,28 +174,65 @@ def menu_basic():
     ]
   }
   """
-  output = {"status": ""}
-  try:
-    vendor_list = []
-    vendors = Vendor.query.all()
-    for vendor in vendors:
-      foods = Food.query.filter_by(vendor_id=vendor.vendor_id, availability=True, listed=True)
-      vendor_json = vendor.json_basic()
-      vendor_json['foods'] = [food.json_basic() for food in foods]
-      vendor_list.append(vendor_json)
+  output = dict()
+  vendor_list = []
+  vendors = Vendor.query.all()
+  for vendor in vendors:
+    foods = Food.query.filter_by(vendor_id=vendor.vendor_id, availability=True, listed=True)
+    vendor_json = vendor.json(0,1)
+    vendor_json['food'] = [food.json(1,2) for food in foods]
+    vendor_list.append(vendor_json)
     output['status'] = "success"
     output['vendors'] = vendor_list
     return jsonify(output), 200
-  except:
-    output['status'] = "error"
-    return jsonify(output), 400
+  
+@app.route("/search/vendor", methods=["POST"])
+def shopfront():
+  if request.is_json:
+    information = request.json
+    try:
+      vendor_id = information['vendor_id']
+      vendor    = Vendor.query.filter_by(vendor_id=vendor_id).first()
+      foods     = Food.query.filter_by(vendor_id=vendor_id,availability=True,listed=True).all()
+      output = {"status": "success"}
+      output.update(dict(vendor.directory()))
+      output.update({"food": [food.json(1,2,3,4,5,6) for food in foods]})
+      return jsonify(output), 200 
+    except:
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
+  else:
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
+    
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400  
 
+@app.route("/search/food", methods=["POST"])
+def purchase():
+  if request.is_json:
+    information = request.json
+    try:
+      vendor_id = information['vendor_id']
+      food_id   = information['food_id']
+      food      = Food.query.filter_by(vendor_id=vendor_id,food_id=food_id,availability=True,listed=True).first()
+      output = {"status": "success"}
+      output.update(food.json(0,1,2,3,4,5,6))
+      return jsonify(output), 200 
+    except:
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
+  else:
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
+    
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400  
+  
+### --- VENDOR APIs --- ###  
 @app.route("/vendor/add", methods=["POST"])
 def add():
-  """
-  Allows a Vendor to add an item to the menu
-  """
-  output = {"status": ""}
+  """ Allows a Vendor to add an item to the menu """
   if request.is_json:
     information = request.json
     try:
@@ -182,73 +240,110 @@ def add():
       food_name        = information['food_name']
       food_description = information['food_description']
       food_price       = information['food_price']
-      db.session.add(Food(vendor_id,food_name,food_description,food_price))
+      food_label       = information.get('food_label', None)
+      db.session.add(Food(vendor_id,food_name,food_description,food_price,food_label))
       db.session.commit()
-      output['status'] = "success"
-      code = 200    
+      return jsonify({"status"     : "success",
+                      "description": "item has been added to the menu"}), 200 
     except:
-      output['status'] = "error"
-      output['message'] = "Error with information processing"
-      code = 400
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
   else:
-    output['status'] = "error"
-    output['message'] = "Posted Information is not JSON"
-    code = 400
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
     
-  return jsonify(output), code
-
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400
+   
 @app.route("/vendor/update", methods=["PUT"])
 def update():
-  """
-  Allows a Vendor to add an item to the menu
-  """
-  output = {"status": ""}
+  """ Allows a Vendor to add an item to the menu """
   if request.is_json:
     information = request.json
     try:
-      food_id        = information['food_id']
+      vendor_id        = information['vendor_id']
+      food_id          = information['food_id']
       food_name        = information['food_name']
       food_description = information['food_description']
       food_price       = information['food_price']
-      food = Food.query.filter_by(food_id=food_id).update({"food_name": food_name,
-                                                           "food_description": food_description,
-                                                           "food_price": food_price})
+      food = Food.query.filter_by(vendor_id=vendor_id,food_id=food_id).update({"food_name"       : food_name,
+                                                                               "food_description": food_description,"food_price"      : food_price})
       db.session.commit()
-      output['status'] = "success"
-      code = 200    
+      return jsonify({"status"     : "success",
+                      "description": "item has been modified in the menu"}), 200 
     except:
-      output['status'] = "error"
-      output['message'] = "Error with information processing"
-      code = 400
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
   else:
-    output['status'] = "error"
-    output['message'] = "Posted Information is not JSON"
-    code = 400
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
     
-  return jsonify(output), code
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400
 
 @app.route("/vendor/delete", methods=["DELETE"])
 def delete():
-  """
-  Allows a Vendor to add an item to the menu
-  """
-  output = {"status": ""}
+  """ Allows a Vendor to delete an item from the menu """
   if request.is_json:
     information = request.json
     try:
-      food_id = information['food_id']
-      food = Food.query.filter_by(food_id=food_id).update({"availability": False,
-                                                           "listed": False})
+      vendor_id = information['vendor_id']
+      food_id   = information['food_id']
+      food = Food.query.filter_by(vendor_id=vendor_id,food_id=food_id).update({"availability": False,
+                                                                               "listed": False})
       db.session.commit()
-      output['status'] = "success"
-      code = 200    
+      return jsonify({"status"     : "success",
+                      "description": "item has been removed from the menu"}), 200 
     except:
-      output['status'] = "error"
-      output['message'] = "Error with information processing"
-      code = 400
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
   else:
-    output['status'] = "error"
-    output['message'] = "Posted Information is not JSON"
-    code = 400
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
     
-  return jsonify(output), code
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400
+
+@app.route("/vendor/take_off", methods=["PUT"])
+def take_off():
+  """ Allows a Vendor to take an item off the menu """
+  if request.is_json:
+    information = request.json
+    try:
+      vendor_id = information['vendor_id']
+      food_id   = information['food_id']
+      food = Food.query.filter_by(vendor_id=vendor_id,food_id=food_id,listed=True).update({"availability": False})
+      db.session.commit()
+      return jsonify({"status"     : "success",
+                      "description": "item has been taken off the menu"}), 200 
+    except:
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
+  else:
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
+    
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400
+
+@app.route("/vendor/put_up", methods=["PUT"])
+def put_up():
+  """ Allows a Vendor to take an item off the menu """
+  if request.is_json:
+    information = request.json
+    try:
+      vendor_id = information['vendor_id']
+      food_id   = information['food_id']
+      food = Food.query.filter_by(vendor_id=vendor_id,food_id=food_id,listed=True).update({"availability": True})
+      db.session.commit()
+      return jsonify({"status"     : "success",
+                      "description": "item has been put onto the menu"}), 200 
+    except:
+      return jsonify({"status"     : "error", 
+                      "description": "bad request"}), 400
+  else:
+    return jsonify({"status"     : "error", 
+                    "description": "posted information is not json"}), 400
+    
+  return jsonify({"status"     : "error", 
+                  "description": "an unknown error occured"}), 400
--- menu/app/model/data_models.py
+++ menu/app/model/data_models.py
@@ -1,5 +1,3 @@
-##!/usr/bin/python
-
 """
 Menu Microservice Data Models
 @Author - Benjamin Wong Wei En, Hao Jun Poon, Belle Lee, Chen Ziyi, Masturah Binte Sulaiman
@@ -8,62 +6,72 @@ Menu Microservice Data Models
 from .base import db
 
 class Vendor(db.Model):
-    vendor_id       = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    vendor_name     = db.Column(db.String(80), nullable=False)
-    halal           = db.Column(db.Boolean, nullable=False)
-    # We point to the Food class
-    menu_foods      = db.relationship("Food", backref="Vendor", lazy=True)
+    vendor_id          = db.Column(db.Integer,    primary_key=True, autoincrement=True)
+    vendor_name        = db.Column(db.String(80), nullable=False)
+    vendor_description = db.Column(db.Text,       nullable=True )
+    vendor_location    = db.Column(db.String(80), nullable=False)
+    vendor_image       = db.Column(db.String(80), nullable=True )
+    menu_foods         = db.relationship("Food",  backref="Vendor", lazy=True)
     
-    def __init__(self,vendor_name,halal):
-        self.vendor_name = vendor_name
-        self.halal       = halal
+    def __init__(self,vendor_name,vendor_description,vendor_location,vendor_image=None):
+        self.vendor_name        = vendor_name
+        self.vendor_description = vendor_description
+        self.vendor_location    = vendor_location
+        self.vendor_image       = vendor_image
+        
+    def directory(self):
+        return [
+            ("vendor_id"         , self.vendor_id),
+            ("vendor_name"       , self.vendor_name),
+            ("vendor_description", self.vendor_description),
+            ("vendor_location"   , self.vendor_location),
+            ("vendor_image"      , self.vendor_image)            
+        ]
     
-    def json_basic(self):
-        return {"vendor_id":   self.vendor_id,
-                "vendor_name": self.vendor_name}        
+    def json(self,*args):
+        output = []
+        values = self.directory()
+        for key in args:
+            output.append(values[key])
+        return dict(output)
     
-    def json_full(self):
-        return {"vendor_id":   self.vendor_id,
-                "vendor_name": self.vendor_name,
-                "halal":       self.halal}
-
 class Food(db.Model):
     vendor_id        = db.Column(db.Integer, db.ForeignKey(Vendor.vendor_id), primary_key=True)
-    food_id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    food_name        = db.Column(db.String(80), nullable=False)
-    food_description = db.Column(db.Text, nullable=True)
+    food_id          = db.Column(db.Integer,            primary_key=True, autoincrement=True)
+    food_name        = db.Column(db.String(80),         nullable=False)
+    food_description = db.Column(db.Text,               nullable=True )
+    food_image       = db.Column(db.String(80),         nullable=True )
     food_price       = db.Column(db.Float(precision=2), nullable=False)
-    # Vendor can take item off an order
-    availability     = db.Column(db.Boolean, nullable=False, default=True)
-    # Vendor deletes the item off the menu
-    listed           = db.Column(db.Boolean, nullable=False, default=True)
+    food_label       = db.Column(db.String(80),         nullable=True )
+    availability     = db.Column(db.Boolean,            nullable=False,   default=True) # take item off an order
+    listed           = db.Column(db.Boolean,            nullable=False,   default=True) # take item off the menu
     
-    def __init__(self,vendor_id,food_name,food_description,food_price):
+    def __init__(self,vendor_id,food_name,food_description,food_price,food_label=None,food_image=None):
         self.vendor_id        = vendor_id
         self.food_name        = food_name
         self.food_description = food_description
+        self.food_image       = food_image
         self.food_price       = food_price
-    
-    def json_default(self):
-        return {"food_id":          self.food_id,
-                "food_name":        self.food_name,
-                "food_description": self.food_description,
-                "food_price":       self.food_price}    
-    
-    def json_basic(self):
-        return {"food_id":          self.food_id,
-                "food_name":        self.food_name}     
-    
-    def json_full(self):
-        return {"vendor_id":        self.vendor_id,
-                "food_id":          self.food_id,
-                "food_name":        self.food_name,
-                "food_description": self.food_description,
-                "food_price":       self.food_price,
-                "availability":     self.availability,
-                "listed":           self.listed}
-        
-    
+        self.food_label       = food_label
 
+    def directory(self):
+        return [
+            ("vendor_id"        , self.vendor_id),
+            ("food_id"          , self.food_id),
+            ("food_name"        , self.food_name),
+            ("food_description" , self.food_description),
+            ("food_image"       , self.food_image),
+            ("food_price"       , self.food_price), 
+            ("food_label"       , self.food_label), 
+            ("availability"     , self.availability), 
+            ("listed"           , self.listed),            
+        ]
     
+    def json(self,*args):
+        output = []
+        values = self.directory()
+        for key in args:
+            output.append(values[key])
+        return dict(output)   
+        
     
