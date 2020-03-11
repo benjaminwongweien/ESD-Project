87577a579263e0917a3e790e212c05093486c35e
LoJo <louis.low.2018@sis.smu.edu.sg>
2020-03-11 13:18:59 +0800

    Minor Updates to complete functionality to get historical orders histroy by id

--- orderprocessing/order/src/main/java/com/deliveryapp/order/OrderController.java
+++ orderprocessing/order/src/main/java/com/deliveryapp/order/OrderController.java
@@ -12,17 +12,33 @@ public class OrderController {
     @Autowired
     OrderRepository OrderRepo;
 
-    @GetMapping("/")
+    @GetMapping("/all")
     public List<Order> index() {
         return OrderRepo.findAll();
     }
 
-    @PostMapping("/retrieve")
-        public List<Order> findOrdersbyUserID(@RequestBody Map<String, String> body) {
-            String custID = body.get("userid");
-            int CustID = Integer.parseInt(custID);
-            return OrderRepo.findByCustomerID(CustID);
+    @PostMapping("/history/customer")
+    public List<Order> findOrdersbyUserID(@RequestBody Map<String, String> body) {
+        String custID = body.get("userid");
+        int CustID = Integer.parseInt(custID);
+        return OrderRepo.findByCustomerID(CustID);
     }
+
+    @PostMapping("/history/vendor")
+    public List<Order> findOrdersbyVendorID(@RequestBody Map<String, String> body) {
+        String vendorID = body.get("vendorid");
+        int venID = Integer.parseInt(vendorID);
+        return OrderRepo.findByvendorID(venID);
+    }
+
+    @PostMapping("/history/deliverer")
+    public List<Order> findOrdersbyDelivererID(@RequestBody Map<String, String> body) {
+        String delID = body.get("delivererid");
+        int delivererID = Integer.parseInt(delID);
+        return OrderRepo.findBydelivererID(delivererID);
+    }
+
+
 
 
 }
