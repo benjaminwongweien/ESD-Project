0cee6deb70ccd2126f5cc7d1dd54285cb8dd175d
benjaminwongweien <54574922+benjaminwongweien@users.noreply.github.com>
2020-04-01 19:15:06 +0800

    Update register.py

--- notification/register/register.py
+++ notification/register/register.py
@@ -67,68 +67,70 @@ def register():
             except:
                 reply_message_id = None
                 
-            if all([message,message_id,sender,reply_message_id]):
+            if all([message,message_id,sender]):
                 # CHECK IF HE HAS ASKED TO REGISTER IN THE DATABASE WITH HIS REPLY MESSAGE ID (1)
-                query       = db.select([Register]).where(Register.columns.message_id==reply_message_id)
-                ResultProxy = connection.execute(query)
-                ResultSet   = ResultProxy.fetchall()
                 
-                if ResultSet:
-                    # CHECK IF USER EXISTS IN CRM
-                    response = requests.post(CRM_USERNAME_GET, json={"username": message})
-                    response = json.loads(response.text)
-                    # (2 fail) REGISTER WITH US ON EASYDELIVERY FIRST!
-                    if response.get("status"):
-                        bot.send_message(msg="Invalid username used to register, resume the registration process by pressing /start", 
-                                        chat_id=sender)
-                    else:
-                        # CHECKS IF USER HAS AN ENTRY IS IN DATAVASE
-                        response = requests.post(CRM_CHATID_GET, json={"tid": sender})
-                        response = json.loads(response.text)      
-                        if not response.get("status"):
-                            response = requests.post(CRM_REGISTER, json={"uid": response["username"],
-                                                                        "tid": None})             
-                        
-                        # IF USER EXISTS REGISTER HIM IN CRM
-                        response = requests.post(CRM_REGISTER, json={"uid": message,
-                                                                    "tid": sender})  
-                        
-                        # REPLY TO USER SUBSCRIPTION SUCCESSFUL
-                        bot.send_message(msg="Registration Successful!", chat_id=sender)
-                        query        = db.delete(Register).where(Register.columns.chat_id==sender)
-                        ResultProxy  = connection.execute(query)
+                if reply_message_id:
+                    query       = db.select([Register]).where(Register.columns.message_id==reply_message_id)
+                    ResultProxy = connection.execute(query)
+                    ResultSet   = ResultProxy.fetchall()
                     
-                # (1 fail) REPLY YOU HAVE REGISTERED BEFORE PLEASE START PROCESS BY WRITING /START
-                else:
-                    bot.send_message(msg="You have not started the reigstration process. Please start by pressing /start.", chat_id=sender)
+                    if ResultSet:
+                        # CHECK IF USER EXISTS IN CRM
+                        response = requests.post(CRM_USERNAME_GET, json={"username": message})
+                        response = json.loads(response.text)
+                        # (2 fail) REGISTER WITH US ON EASYDELIVERY FIRST!
+                        if response.get("status"):
+                            bot.send_message(msg="Invalid username used to register, resume the registration process by pressing /start", 
+                                            chat_id=sender)
+                        else:
+                            # CHECKS IF USER HAS AN ENTRY IS IN DATAVASE
+                            response = requests.post(CRM_CHATID_GET, json={"tid": sender})
+                            response = json.loads(response.text)      
+                            if not response.get("status"):
+                                response = requests.post(CRM_REGISTER, json={"uid": response["username"],
+                                                                            "tid": None})             
+                            
+                            # IF USER EXISTS REGISTER HIM IN CRM
+                            response = requests.post(CRM_REGISTER, json={"uid": message,
+                                                                        "tid": sender})  
+                            
+                            # REPLY TO USER SUBSCRIPTION SUCCESSFUL
+                            bot.send_message(msg="Registration Successful!", chat_id=sender)
+                            query        = db.delete(Register).where(Register.columns.chat_id==sender)
+                            ResultProxy  = connection.execute(query)
+                        
+                    # (1 fail) REPLY YOU HAVE REGISTERED BEFORE PLEASE START PROCESS BY WRITING /START
+                    else:
+                        bot.send_message(msg="You have not started the reigstration process. Please start by pressing /start.", chat_id=sender)
 
             
-            elif message == "/start":
-                # CHECK IF REGISTRATION REQUEST WAS MADE BEFORE
-                query       = db.select([Register]).where(Register.columns.chat_id==sender)
-                ResultProxy = connection.execute(query)
-                ResultSet   = ResultProxy.fetchall()
-                if ResultSet:
-                    bot_response = bot.message_reply("You have already attempted to register, please reply to this message to continue.", sender)
-                else:
-                    # CHECK IF USER's CHAT ID IS IN DATABASE
-                    response = requests.post(CRM_CHATID_GET, json={"tid": sender})
-                    response = json.loads(response.text)
-                                
-                    if response.get("status"):
-                        bot_response = bot.message_reply("Thank you for subscribing to EaSy Delivery! If you are a new user, please register by replying to this message.", sender)
+                elif message == "/start":
+                    # CHECK IF REGISTRATION REQUEST WAS MADE BEFORE
+                    query       = db.select([Register]).where(Register.columns.chat_id==sender)
+                    ResultProxy = connection.execute(query)
+                    ResultSet   = ResultProxy.fetchall()
+                    if ResultSet:
+                        bot_response = bot.message_reply("You have already attempted to register, please reply to this message to continue.", sender)
                     else:
-                        # IF NOT IN DATABASE
-                        bot_response = bot.message_reply("This telegram id is already registered. To reassociate your telegram account, please reply to this message with your new username.", sender)
-                
-                reply_to_message_id = bot_response["result"]["message_id"]
-                
-                if ResultSet:
-                    query = db.update(Register).values(message_id=reply_to_message_id).where(Register.columns.chat_id==sender)
-                else:
-                    query = db.insert(Register).values(chat_id=sender, message_id=reply_to_message_id)
+                        # CHECK IF USER's CHAT ID IS IN DATABASE
+                        response = requests.post(CRM_CHATID_GET, json={"tid": sender})
+                        response = json.loads(response.text)
+                                    
+                        if response.get("status"):
+                            bot_response = bot.message_reply("Thank you for subscribing to EaSy Delivery! If you are a new user, please register by replying to this message.", sender)
+                        else:
+                            # IF NOT IN DATABASE
+                            bot_response = bot.message_reply("This telegram id is already registered. To reassociate your telegram account, please reply to this message with your new username.", sender)
                     
-                ResultProxy = connection.execute(query)
+                    reply_to_message_id = bot_response["result"]["message_id"]
+                    
+                    if ResultSet:
+                        query = db.update(Register).values(message_id=reply_to_message_id).where(Register.columns.chat_id==sender)
+                    else:
+                        query = db.insert(Register).values(chat_id=sender, message_id=reply_to_message_id)
+                        
+                    ResultProxy = connection.execute(query)
 
 
 #############################
